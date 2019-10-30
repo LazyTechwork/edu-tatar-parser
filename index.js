ab2eb8211baf815d73366f1ba5b1fa14e5562fee
Semenov Roman <3908441@gmail.com>
2019-10-30 09:55:19 +0300

    laravel template

--- index.js
+++
@@ -1,7 +0,0 @@
-const App = require("./src/app.js");
-const app = new App({
-	port: process.env.PORT || 8000,
-	eduTatarUrl: "https://edu.tatar.ru"
-});
-
-app.listen();
--- src/exceptions.js
+++
@@ -1,9 +0,0 @@
-module.exports.NoCredentialsException = function() {
-	this.message = "No credentials. Make sure you've specified `login` and `password` fields";
-	this.statusCode = 401;
-};
-
-module.exports.WrongCredentialsException = function() {
-	this.message = "Login or password is incorrect";
-	this.statusCode = 403;
-};
